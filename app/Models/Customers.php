280c7763e1bc7685289b65e8a594b497da698563
nguyenvankhanhdev <145115592+nguyenvankhanhdev@users.noreply.github.com>
2024-09-06 16:02:49 +0700

    create model

---
+++ app/Models/Customers.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customers extends Model
+{
+    use HasFactory;
+    public function address(){
+        return $this->hasMany(Address::class, 'user_id');
+    }
+    public function account(){
+        return $this->belongsTo(Accounts::class, 'user_id');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BackEnd\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('products', HomeController::class);
+
